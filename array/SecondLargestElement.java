cb67f84e31979eae4743a5930785c1b494df258d
RAJ <Anujpatiyal@gmail.com>
2024-07-01 04:49:40 +0530

    Find second largest element from a given array

---
+++ array/SecondLargestElement.java
@@ -0,0 +1,21 @@
+class SecondLargestElement
+{
+    public static void main(String[] args)
+    {
+        int temp = 0;
+        int[] a = {3,5,1,4,2,7};
+        for(int i=0;i<a.length;i++)
+        {
+            for(int j=i+1;j<a.length;j++)
+            {
+                if(a[i]>a[j])
+                {
+                    temp=a[i];
+                    a[i]=a[j];
+                    a[j]=temp;
+                }
+            }
+        }
+        System.out.println("Second largest element : "+a[a.length-2]);
+    }
+}
